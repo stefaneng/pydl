63aa34db4537f5adbaab835313b3ed10b81efec2
Stefan Eng <stefaneng13@gmail.com>
2013-02-24 18:53:14 -0800

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='PyDl',
+    version='0.1dev',
+    packages=['pydl'],
+    author='Stefan Eng',
+    author_email='stefaneng13@gmail.com',
+    license='',
+    long_description=open('README').read(),
+)
