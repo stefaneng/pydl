4f66b9eb9ded6b3a34d1efb00c8210557f6f45bd
Stefan Eng <stefaneng13@gmail.com>
2013-03-09 23:25:04 +0000

    Fixed up readme and changed --prompt flag to --quit

--- README.rst
+++ README.rst
@@ -1,4 +1,22 @@
 PyDl
+====
+
+Python Downloader v.01dev
 ----
+
+- Purpose:
+----
+Downloads all files of an extention off of a website.
+
+- Use:
+----
+pydl [-flags] url extention
+
+- Flags:
+----
+-v  --verbose    Prints out more information during execution
+-q  --quiet      Does prompt the user to accept downloads or name files		
+
+
 
 
--- pydl/pydl.py
+++ pydl/pydl.py
@@ -9,43 +9,49 @@ from os.path import basename
 from bs4 import BeautifulSoup
 from urllib2 import urlopen, URLError, HTTPError
 
-# Check out shutil
-# shutil.copyfileobj(source, destination)
-
-def get_links(url, ext):
+def get_links(url, ext, verbose=False):
     """
-    Takes in a string for the url and a string for the target extention
+    Takes in a string for the url and a string for the target extention.
     """
     try:
         # Reads the url
         html = urlopen(url)
+        # Gets the html in a better format
         soup = BeautifulSoup(html)
         # Returns the found links matching extention
         all_links = soup.find_all('a')
-        regex = re.compile ('.*\.' + ext)
+        regex = re.compile('.*\.' + ext)
         new_links = [x for x in all_links 
                      if regex.search(x.get('href'))]
-        for i in new_links:
-            print i
-        # return all_links
+        # Print all information if user wants it
+        if verbose:
+            print 'Matching extention: ' + ext
+            print '---------------------------'
+            for i in new_links:
+                print i.get('href')
+
         return new_links
-        # return [x for x in all_links if re.search('.*\.' + ext, x)]
 
     except HTTPError, e:
         print "HTTP Error: ", e.code, url
     except URLError, e:
         print "URL Error: ", e.code, url
 
-def save_link(url, prompt=False):
+def save_link(url, filename=None):
+    '''
+    Saves a link from a url to a filename
+    Default value is the original filename from the website
+    '''
     try:
+        # Opens the url for reading
         link = urlopen(url)
-        # Test file name
-        # Name it with link.info()['Content-Disposition'] ?
-        f = open(basename(url), 'w+')
-        f.write(link.read())
-        f.close()
-        #with open(os.path.basename(link),'w+b') as f:
-        #    f.write(link.read())
+        if filename:
+            name = filename
+        else:
+            name = basename(url)
+        with open(name, 'w+') as f:
+            print 'Downloading......' + basename(url)
+            f.write(link.read())
     except HTTPError, e:
         print "HTTP Error: ", e.code, url
     except URLError, e:
@@ -58,25 +64,27 @@ def main():
     parser.add_argument('ext', help='extention of file to download')
     parser.add_argument('-v', '--verbose', help='display what is going on',
                         action='store_true')
-    parser.add_argument('-p', '--prompt', 
-                        help='prompt before saving files and ask for filename',
+    parser.add_argument('-q', '--quiet', 
+                        help='silences prompts and saves as original filename',
                         action='store_true')
     args = parser.parse_args()
     links = get_links(args.url, args.ext)
+
+    # User wants more output
     if args.verbose:
         for x in links:
-            print os.path.basename(x)
+            print basename(x.get('href'))
 
     for i in links:
-        # Use wants to be prompted
-        if args.prompt:
-            save_link(i.get('href'), True)
-        else:
+        # Use does not want to be prompted, saves files with default name
+        if args.quiet:
             save_link(i.get('href'))
-
-#    print save_link("http://www.elsevierdirect.com/downloads/SyngressFreeE-booklets/Certification/1597491535.pdf")
-    #print os.path.basename("/home/stefan/.xmonad/xmonad.hs")
-
+        else:
+            print "Do you want to save: " + basename(i.get('href')) + " ?"
+            var = raw_input("[y/n]: ")
+            if var == 'y':
+                var = raw_input("Rename to: ")
+                save_link(i.get('href'),var)
 
 if __name__ == '__main__':
     main()
